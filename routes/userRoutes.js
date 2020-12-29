3f6f80c7d670f97e784cf3a49bc6f1d294ff4e58
Preetam Sarkar <sarkarpreetam12@gmail.com>
2020-12-30 01:37:30 +0530

    ejs url update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,11 @@
 const { Router } = require('express')
-const uController = require('../controllers/userController')
+const uController = require('../views/userController')
 
 const router = Router()
 
 router.post('/signup',uController.signup_post)
 router.post('/login',uController.login_post)
 router.get('/logout', uController.logout_get);
-
+router.get('/about', uController.about);
 
 module.exports = router;
--- views/userController.js
+++ views/userController.js
@@ -62,4 +62,8 @@ module.exports.login_post = async (req,res)=> {
 module.exports.logout_get = (req, res) => {
     res.cookie('jwt', '', { maxAge: 1 });
     res.redirect('/');
-  }
+}
+
+module.exports.about = (req, res) => {
+    res.render("about")
+}
