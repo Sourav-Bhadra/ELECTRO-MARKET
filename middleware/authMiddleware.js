048bbb2f277f1428f59598f44cd12c6a4e943d5e
Preetam Sarkar <sarkarpreetam12@gmail.com>
2021-01-09 01:16:35 +0530

    done 90% of login and signup

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,25 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
+const requireAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  // check json web token exists & is verified
+  if (token) {
+    jwt.verify(token, 'Hack CW', (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.redirect('/login');
+      } else {
+        console.log(decodedToken);
+        next();
+      }
+    });
+  } else {
+    res.redirect('/login');
+  }
+};
+
 // check current user
 const checkUser = (req, res, next) => {
     const token = req.cookies.jwt;
@@ -22,4 +41,4 @@ const checkUser = (req, res, next) => {
   };
 
 
-  module.exports = { checkUser}
+  module.exports = { requireAuth ,checkUser}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express')
 const uController = require('../views/userController')
+const { requireAuth, checkUser } = require('../middleware/authMiddleware');
 
 const router = Router()
 
@@ -7,5 +8,8 @@ router.post('/signup',uController.signup_post)
 router.post('/login',uController.login_post)
 router.get('/logout', uController.logout_get);
 router.get('/about', uController.about);
+router.get('/account', uController.account);
+router.get('/products',requireAuth, uController.product);
+router.get('/projects',requireAuth, uController.project);
 
 module.exports = router;
